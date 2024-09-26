bd683e13abbd649d0d27919e8827359601ce9299
Anne Redulla <aredulla@google.com>
2024-09-26 13:46:04 +1000

    Prefer direct git installation for Windows
    Bug: b/360206460
    Change-Id: I7068b201b0b976ac619db16ff16d5ffbe5aea362

--- bootstrap/git-bash.template.sh
+++ bootstrap/git-bash.template.sh
@@ -2,11 +2,12 @@
 export EDITOR=${EDITOR:=notepad}
 WIN_BASE=`dirname $0`
 UNIX_BASE=`cygpath "$WIN_BASE"`
+UNIX_GIT_BIN_ABSDIR=`cygpath "${GIT_BIN_ABSDIR}"`
 export PATH="$PATH:$UNIX_BASE/${PYTHON3_BIN_RELDIR_UNIX}:$UNIX_BASE/${PYTHON3_BIN_RELDIR_UNIX}/Scripts"
 export PYTHON_DIRECT=1
 export PYTHONUNBUFFERED=1
 if [[ $# > 0 ]]; then
-  $UNIX_BASE/${GIT_BIN_RELDIR_UNIX}/bin/bash.exe "$@"
+  $UNIX_GIT_BIN_ABSDIR/bin/bash.exe "$@"
 else
-  $UNIX_BASE/${GIT_BIN_RELDIR_UNIX}/git-bash.exe &
+  $UNIX_GIT_BIN_ABSDIR/git-bash.exe &
 fi
