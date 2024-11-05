637ef18a336ae6636e4d7c660d6cd26ee483ab6d
Anne Redulla <aredulla@google.com>
2024-11-04 23:18:24 -0800

    [git-sso] Read sso helper output line by line
    On Windows, the subprocess running the sso helper does not
    complete the `read()` in time. This CL reads the output
    line by line, and stops reading once the required fields
    have been specified.
    
    Bug: b/360206460
    Change-Id: Ib5f95093cd6f9bbbe5093a7e16393ecd97934cf2

--- gerrit_util.py
+++ gerrit_util.py
@@ -352,6 +352,11 @@ class SSOAuthenticator(_Authenticator):
     # Overridden in tests.
     _timeout_secs = 5
 
+    # The required fields for SSO configuration, expected from the sso helper's
+    # output.
+    _required_config_fields = frozenset(
+        ['http.cookiefile', 'http.proxy', 'include.path'])
+
     @dataclass
     class SSOInfo:
         proxy: httplib2.ProxyInfo
@@ -388,6 +393,12 @@ class SSOAuthenticator(_Authenticator):
 
     @classmethod
     def _parse_config(cls, config: str) -> SSOInfo:
+        """Parses the sso config from the given string.
+
+        Note: update cls._required_config_fields appropriately when making
+        changes to this method, to ensure the field values are captured
+        from the sso helper.
+        """
         parsed: Dict[str, str] = dict(line.strip().split('=', 1)
                                       for line in config.splitlines())
 
@@ -466,14 +477,26 @@ class SSOAuthenticator(_Authenticator):
                     timer = threading.Timer(cls._timeout_secs, _fire_timeout)
                     timer.start()
                     try:
-                        stdout_data = proc.stdout.read()
+                        # Keep track of which config settings have been read.
+                        fields = set()
+                        stdout_data = ''
+                        for line in proc.stdout:
+                            if not line:
+                                break
+                            stdout_data += line
+                            fields.add(line.split('=', 1)[0])
+                            # Stop reading if we have all the required fields.
+                            if fields >= cls._required_config_fields:
+                                break
                     finally:
                         timer.cancel()
 
-                    if timedout:
-                        LOGGER.error(
-                            'SSOAuthenticator: Timeout: %r: reading config.',
-                            cmd)
+                    missing_fields = cls._required_config_fields - fields
+                    if timedout or missing_fields:
+                        message = (
+                            'SSOAuthenticator: Timeout: %r: config still '
+                            'missing fields [%s].')
+                        LOGGER.error(message, cmd, ', '.join(missing_fields))
                         raise subprocess.TimeoutExpired(
                             cmd=cmd, timeout=cls._timeout_secs)
 
