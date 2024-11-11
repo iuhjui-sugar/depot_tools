410853d7894cb1b34253b77bf40b7fd7e3484c47
Anne Redulla <aredulla@google.com>
2024-11-11 14:30:43 -0800

    Prefer direct git installation for Windows
    Bug: b/360206460
    Change-Id: I7068b201b0b976ac619db16ff16d5ffbe5aea362

--- bootstrap/git.template.bat
+++ bootstrap/git.template.bat
@@ -1,5 +1,8 @@
 @echo off
 setlocal
 if not defined EDITOR set EDITOR=notepad
-set PATH=%~dp0${GIT_BIN_RELDIR}\cmd;%~dp0;%PATH%
-"%~dp0${GIT_BIN_RELDIR}\${GIT_PROGRAM}" %*
+:: Exclude the current directory and limit the search for executables
+:: to PATH. This is required for the SSO helper to run.
+set "NoDefaultCurrentDirectoryInExePath=1"
+set "PATH=${GIT_BIN_ABSDIR}\cmd;%~dp0;%PATH%"
+"${GIT_BIN_ABSDIR}\${GIT_PROGRAM}" %*
